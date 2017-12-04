bfeebf8e7c54be91323e05a38eb1eaedd477abf6
OXOYO <zmn2007.hi@163.com>
2017-12-04 13:07:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## X-Compress-Images
+Minify size your images by [compress-images](https://github.com/semiromid/compress-images)
+
+## Start
+```bash
+    npm i
+    gulp
+```
+
+## Docs
+[compress-images](https://github.com/semiromid/compress-images)
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/**
+ * Created by OXOYO on 2017/12/4.
+ */
+
+const gulp = require('gulp')
+const compress_images = require('compress-images')
+
+const INPUT_path_to_your_images = 'src/img/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}'
+const OUTPUT_path = 'build/img/'
+
+compress_images (
+    INPUT_path_to_your_images,
+    OUTPUT_path,
+    {
+        compress_force: false,
+        statistic: true,
+        autoupdate: true
+    },
+    false,
+    {jpg: {engine: 'mozjpeg', command: ['-quality', '60']}},
+    {png: {engine: 'pngquant', command: ['--quality=20-50']}},
+    {svg: {engine: 'svgo', command: '--multipass'}},
+    {gif: {engine: 'gifsicle', command: ['--colors', '64', '--use-col=web', '--scale', '0.2']}},
+    function(){}
+)
+
+
+gulp.task('default', compress_images)
